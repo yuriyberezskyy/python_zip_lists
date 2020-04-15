deb33af5fc05049ee8660fe23165019bb2398163
yuriyberezskyy <bereza95@gmail.com>
2020-04-15 19:18:23 -0400

    done

---
+++ zip.py
@@ -0,0 +1,5 @@
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+
+names_and_dogs_names = zip(names,dogs_names)
+print(list(names_and_dogs_names))
